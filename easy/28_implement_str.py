e4f71453b8325ddc1b1a8ed7745ba2b40563cde4
kylee <leeky940926@gmail.com>
2022-01-07 16:36:29 +0900

    [ADD] success implement_str problem

--- easy/28_implement_str.py
+++ easy/28_implement_str.py
@@ -1,6 +1,3 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle == "" : return 0
-        if haystack == "" : return -1
-        
-        return haystack.find(needle)
+        return 0 if needle == "" else haystack.find(needle) 
